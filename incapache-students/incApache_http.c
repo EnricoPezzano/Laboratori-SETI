89ab6d9c419604b8191e6ee7ad87f72c9f77ab38
Enrico Pezzano <enrico@MacBook-Air-di-Enrico.local>
2022-01-07 18:34:25 +0100

    d

--- incapache-students/incApache_http.c
+++ incapache-students/incApache_http.c
@@ -420,15 +420,16 @@ void manage_http_requests(int client_fd
 // 	}
 // }
 
-	char *iduser = "UserID=";
+	char *iduser = "UserID="; // old, stampa set-cookie, ma non client provided UID Cookie x for the x time
 	option_val = strtok_r(NULL, "\r\n", &strtokr_save);
 	//remove blank spaces
 	while(option_val != NULL && *option_val == ' ')
 			option_val++;
 
-	if(option_val != NULL && !strncmp(option_val,iduser,strlen(iduser)))
-		sscanf(option_val + strlen(iduser),"%d",&UIDcookie);
+	// if(option_val != NULL && !strncmp(option_val,iduser,strlen(iduser)))
+		// sscanf(option_val + strlen(iduser),"%d",&UIDcookie);
 
+	// new, stampa client provided UID Cookie x for the x time, ma non set-cookie...
 	// strtok_r(NULL, " \n\r=", &strtokr_save); // '\r' è il ritorno a capo, i separatori sono ' ', \n, = e \r
 	UIDcookie = atoi(strtokr_save); // client provided UID Cookie 1 for the 1 time
 
