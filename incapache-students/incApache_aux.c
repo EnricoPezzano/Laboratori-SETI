1569a0844a317537a76dad8db6674792f2ded0ee
Enrico Pezzano <enrico@MacBook-Air-di-Enrico.local>
2022-01-07 02:20:11 +0100

    d

--- incapache-students/incApache_aux.c
+++ incapache-students/incApache_aux.c
@@ -59,7 +59,7 @@ time_t my_timegm(struct tm *tm)
 /*** TO BE DONE 5.0 START ***/
 
     pthread_mutex_lock(&my_timegm_mutex);
-    tz = getenv("TZ"); // vedi man
+    // tz = getenv("TZ"); // vedi man // only last year difference
 
 /*** TO BE DONE 5.0 END ***/
 
