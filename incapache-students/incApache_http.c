bc1066fcde7c444c3211fdf20ead60389ac675b5
Enrico Pezzano <enrico@MacBook-Air-di-Enrico.local>
2022-01-08 18:10:37 +0100

    d

--- incapache-students/incApache_http.c
+++ incapache-students/incApache_http.c
@@ -426,12 +426,12 @@ void manage_http_requests(int client_fd
 	while(option_val != NULL && *option_val == ' ')
 			option_val++;
 
-	// if(option_val != NULL && !strncmp(option_val,iduser,strlen(iduser)))
-		// sscanf(option_val + strlen(iduser),"%d",&UIDcookie);
+	if(option_val != NULL && !strncmp(option_val,iduser,strlen(iduser)))
+		sscanf(option_val + strlen(iduser),"%d",&UIDcookie);
 
 	// new, stampa client provided UID Cookie x for the x time, ma non set-cookie...
 	// strtok_r(NULL, " \n\r=", &strtokr_save); // '\r' è il ritorno a capo, i separatori sono ' ', \n, = e \r
-	UIDcookie = atoi(strtokr_save); // client provided UID Cookie 1 for the 1 time
+	// UIDcookie = atoi(strtokr_save); // client provided UID Cookie 1 for the 1 time
 
 /*** TO BE DONE 5.0 END ***/
 
