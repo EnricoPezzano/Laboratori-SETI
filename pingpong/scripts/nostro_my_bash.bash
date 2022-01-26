b7501ae194935fd083483f69a03a91a346b85f45
Enrico Pezzano <enrico@MacBook-Air-di-Enrico.local>
2022-01-26 14:28:02 +0100

    d

--- pingpong/scripts/nostro_my_bash.bash
+++ pingpong/scripts/nostro_my_bash.bash
@@ -1,44 +1,12 @@
 #!/bin/bash 
-#è una direttiva 
+#...è una direttiva 
 # "#!" è detto shebang, /bin/bash indica l'interprete di comandi usato, in questo caso proprio bash
 
 # 1. Leggere dal file throughput.dat (generato mediante una interazione con il server 
 # Pong su webdev.disi.unige.it) la prima e l'ultima riga ed estrarre i valori N1 e N2,
 # T(N1) e T(N2). Si vedano i comandi head e tail per leggere le righe dal file.
-set -e
+#lillo
 
-#controllo se i file .dat sono presenti nella cartella data
-if [ ! -f ../data/tcp_throughput.dat ]; then
-    printf "\nUnable to find tcp_throughput source file\n\n"
-    exit 1
-fi
-
-if [ ! -f ../data/udp_throughput.dat ]; then
-    printf "\nUnable to find udp_throughput source file\n\n"
-    exit 1
-fi
-
-#crea variabile di sola lettura
-readonly InputTCPFile="../data/tcp_throughput.dat"
-
-#creo variabili con prima e ultima riga del file input
-declare -a firsttcp=($(head -n 1 ${InputTCPFile}))  
-declare -a lasttcp=($(tail -n 1 ${InputTCPFile}))
-
-#crea due interi in cui mette il primo numero di ciascuna riga
-declare -i n1tcp=${firsttcp[0]}
-declare -i n2tcp=${lasttcp[0]}
-
-#variabile che contiene il secondo numero di ciascuna riga
-declare -a mediantcp1=${firsttcp[2]}
-declare -a mediantcp2=${lasttcp[2]}
-
-#n1tcp/mediantcp
-#bc -> fai operazione matematica
-#-l -> libreria mathlib
-#D = N/T
-declare delayn1=$(bc -l <<< "($n1tcp/$mediantcp1)")
-declare delayn2=$(bc -l <<< "($n2tcp/$mediantcp2)")
 
 
 
@@ -49,11 +17,19 @@ declare delayn2=$(bc -l <<< "($n2tcp/$mediantcp2)")
 # solo variabili di tipo intero o di tipo stringa). Si suggerisce quindi di lanciare l'applicazione bc (vedi 
 # man bc) che permette di effettuare calcoli in virgola mobile con precisione arbitrariamente predefinita (e di 
 # effettuare calcoli con una precisione di almeno 9 cifre decimali).
+#mio anno scorso
+#calcolo le costanti
+echo 'calcolo costanti'
+# il delay è legato al thorughtput medio e al numero di byte dalla formula T*D=N -> D=N/T
+DelayMin=$(bc <<<"scale=20;var1=${N1};var2=${T1};var1 / var2")
+DelayMax=$(bc <<<"scale=20;var1=${N2};var2=${T2};var1 / var2")
+Denominatore=$(bc <<< "scale=20;${DelayMax}-${DelayMin}")
+
 L0=$(bc <<< "scale=20;var1=${DelayMin}*${N2};var2=${DelayMax}*${N1};var3=var1-var2;var3/${Denominatore}")
 B=$(bc <<< "scale=20;var1=${N2}-${N1};var1/${Denominatore}")
 
 echo bandwiwidth: $B
-echo latency: $L
+echo latency: $L0
 #stampa i valori Numero_byte e Latenza sul file .dat
 echo 'stampo i valori Numero_byte e Latenza sul file .dat'
 N_LINEE_FILE=$(wc -l "${DataDir}/${ProtocolName}_throughput.dat" | cut -d ' ' -f1)
@@ -82,6 +58,7 @@ echo "B0=$B0"
 # essere mediante la definizione di una funzione, per esempio:
 #   lbf(x) = x / ( $myL + x / $myB )
 #       plot lbf(x) title "Latency-Bandwidth model with L=$myL and B=$myB" with linespoints
+#twingo
 gnuplot <<-eNDgNUPLOTcOMMAND
     set term png size 900,700
     
